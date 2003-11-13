b4c1931fb84676655193f2845662dac928951b4c
Peter Marklund <peter_marklund@fastmail.fm>
2003-11-13 13:32:39 +0000

    finishing the move of character-role mapping from simbuild to siminst

--- lib/sim-template-roles.tcl
+++ lib/sim-template-roles.tcl
@@ -44,10 +44,6 @@ template::list::create \
         name { 
             label "Name"
         }
-        char_name {
-            label "Character"
-            link_url_col char_url
-        }
         delete {
             sub_class narrow
             link_url_col delete_url
@@ -63,19 +59,13 @@ set return_url "[ad_conn url]?[ad_conn query]"
 db_multirow -extend { edit_url char_url delete_url } roles select_roles "
     select wr.role_id,
            wr.pretty_name as name,
-           wr.sort_order,
-           cr.title as char_name
+           wr.sort_order
       from workflow_roles wr,
-           sim_roles sr,
-           cr_items ci,
-           cr_revisions cr
+           sim_roles sr
      where wr.workflow_id = :workflow_id
        and sr.role_id = wr.role_id
-       and ci.item_id = sr.character_id
-       and cr.revision_id = ci.live_revision
     [template::list::orderby_clause -orderby -name "roles"]
 " {
     set edit_url [export_vars -base "role-edit" { role_id }]
-    set char_url [export_vars -base "object/$char_name"]
     set delete_url [export_vars -base "role-delete" { role_id return_url }]
 }
