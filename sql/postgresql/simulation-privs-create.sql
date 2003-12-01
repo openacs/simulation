94791d7f4c553e47450a4d9bf4d12310776baec4
Simon Carstensen <simon@bcuni.net>
2003-12-01 15:15:08 +0000

    updated privs to new structure

--- sql/postgresql/simulation-privs-create.sql
+++ sql/postgresql/simulation-privs-create.sql
@@ -1,4 +1,13 @@
--- privileges
+-- privileges 
+select acs_privilege__create_privilege('sim_admin','',null);
+select acs_privilege__create_privilege('sim_template_create','',null);
+select acs_privilege__create_privilege('sim_inst','',null);
+select acs_privilege__create_privilege('sim_set_map_p','',null);
+select acs_privilege__create_privilege('sim_object_writer','',null);
+select acs_privilege__create_privilege('sim_object_create','',null);
+select acs_privilege__create_privilege('sim_object_write','',null);
+
+-- add children
 select acs_privilege__add_child('admin','sim_admin');
 select acs_privilege__add_child('sim_admin','sim_template_create');
 select acs_privilege__add_child('sim_admin','sim_inst');
