dc4e9973f618e7519bc492c975379a975c471b2c
Joel Aufrecht <joel@aufrecht.org>
2003-12-18 14:07:02 +0000

    fixed missing variables

--- www/simplay/index.tcl
+++ www/simplay/index.tcl
@@ -1,11 +1,12 @@
 ad_page_contract {
     Simplay index page.
 } {
-    case_id:integer,optional
+    {case_id:integer ""}
 }
 
 set title "SimPlay"
 set context [list $title]
+set user_id [ad_conn user_id]
 set package_id [ad_conn package_id]
 set section_uri [apm_package_url_from_id $package_id]simplay/
 
