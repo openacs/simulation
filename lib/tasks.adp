cb20575ffd4fdfaf34e0558a99cd31d73ccdc5d7
Lars Pind <lars@pinds.com>
2004-01-06 09:51:25 +0000

    Show role on tasks; show when case is completed

--- lib/tasks.adp
+++ lib/tasks.adp
@@ -1,1 +1,6 @@
-<listtemplate name="tasks"></listtemplate></p>
+<if @complete_p@ true>
+  <i>This case has been completed.</i>
+</if>
+<else>
+  <listtemplate name="tasks"></listtemplate></p>
+</else>
--- lib/tasks.tcl
+++ lib/tasks.tcl
@@ -17,12 +17,33 @@ if { [empty_string_p $case_id] } {
     unset case_id
 }
 
+if { [exists_and_not_null case_id] } {
+    
+    set num_enabled_actions [db_string select_num_enabled_actions { 
+        select count(*) 
+        from   workflow_case_enabled_actions 
+        where  case_id = :case_id
+        and    enabled_state = 'enabled'
+    }]
+
+    set complete_p [expr $num_enabled_actions == 0]
+
+    set user_roles [workflow::case::get_user_roles -case_id $case_id]
+} else {
+    set user_roles [list]
+    set complete_p 0
+}
+
 set package_id [ad_conn package_id]
 
 set elements {
     name {
         link_url_col task_url
         label "Task"
+    }
+    role {
+        label "Role"
+        hide_p {[ad_decode [llength $user_roles] 1 1 0]}
     }
     case_label {
         label "Case"
@@ -45,20 +66,21 @@ template::list::create \
         }
     }
 
-# TODO: make case_name be a combo of simulation name and case #
 db_multirow -extend { task_url } tasks select_tasks "
     select wcea.enabled_action_id,
            wa.pretty_name as name,
            wcea.case_id,
            sc.label as case_label,
-           w.pretty_name as sim_name
+           w.pretty_name as sim_name,
+           wr.pretty_name as role
       from workflow_case_enabled_actions wcea,
            workflow_case_role_party_map wcrmp,
            workflow_actions wa,
            party_approved_member_map pamm,
            workflow_cases wc,
            sim_cases sc,
-           workflows w
+           workflows w,
+           workflow_roles wr
      where wcea.enabled_state = 'enabled'
        and pamm.member_id = :user_id
        and wcrmp.party_id = pamm.party_id
@@ -68,6 +90,7 @@ db_multirow -extend { task_url } tasks select_tasks "
        and wc.case_id = wcea.case_id
        and sc.sim_case_id = wc.object_id
        and w.workflow_id = wc.workflow_id
+       and wr.role_id = wa.assigned_role
     [template::list::filter_where_clauses -and -name "tasks"]
     order by wa.sort_order
 " {
