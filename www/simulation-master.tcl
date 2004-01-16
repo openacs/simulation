0eb72e16e367ef98eedc6a4b27bab6e2eea5ae75
Lars Pind <lars@pinds.com>
2004-01-16 15:44:52 +0000

    Fixed TODO (.25h): kill link bar for players

--- www/simulation-master.tcl
+++ www/simulation-master.tcl
@@ -63,45 +63,47 @@ if { ![empty_string_p [ad_conn extra_url]] } {
 #
 ######################################################################
 
-#  TODO (.25h): kill link bar for players
+if { !$citybuild_p && !$simbuild_p && !$siminst_p && !$admin_p } {
+    set subnavbar_link {}
+} else {
+    if { $citybuild_p } {
+        lappend subnavbar_list [list "${base_url}citybuild" "CityBuild"]
+    }
 
-if { $citybuild_p } {
-    lappend subnavbar_list [list "${base_url}citybuild" "CityBuild"]
+    if { $simbuild_p } {
+        lappend subnavbar_list [list "${base_url}simbuild" "SimBuild"]
+    }
+
+    if { $siminst_p } {
+        lappend subnavbar_list [list "${base_url}siminst" "SimInst"]
+    }
+
+    if { ![info exists header_stuff] } {
+        set header_stuff {}
+    }
+
+    lappend subnavbar_list [list "${base_url}simplay" "SimPlay"]
+
+    if { $admin_p } {
+        lappend subnavbar_list [list $parameters_url Configuration] 
+        lappend subnavbar_list [list "/test/admin/index?by_package_key=simulation&view_by=testcase&quiet=0" Tests]
+    }
+
+    lappend subnavbar_list [list "/doc/simulation" "Doc"]
+
+    # TODO B: should use ad_navbar
+    # couldn't figure out how to pass the input to ad_narbar so hacking it in here
+    # and also added context checking
+    set link_list ""
+    foreach arg $subnavbar_list {
+            lappend link_list "<a href=\"[lindex $arg 0]\">[lindex $arg 1]</a>"
+    # removed context checking because people keep trying to use this navbar to 
+    # get to the local parent, which context checking prevents
+    #    if { [string match *[lindex $arg 0]* [ad_conn url]] } {
+    #        lappend link_list "[lindex $arg 1]"
+    #    } else {
+    #        lappend link_list "<a href=\"[lindex $arg 0]\">[lindex $arg 1]</a>"
+    #    }
+    }
+    set subnavbar_link "\[[join $link_list " | "]\]"
 }
-
-if { $simbuild_p } {
-    lappend subnavbar_list [list "${base_url}simbuild" "SimBuild"]
-}
-
-if { $siminst_p } {
-    lappend subnavbar_list [list "${base_url}siminst" "SimInst"]
-}
-
-if { ![info exists header_stuff] } {
-    set header_stuff {}
-}
-
-lappend subnavbar_list [list "${base_url}simplay" "SimPlay"]
-
-if { $admin_p } {
-    lappend subnavbar_list [list $parameters_url Configuration] 
-    lappend subnavbar_list [list "/test/admin/index?by_package_key=simulation&view_by=testcase&quiet=0" Tests]
-}
-
-lappend subnavbar_list [list "/doc/simulation" "Doc"]
-
-# TODO B: should use ad_navbar
-# couldn't figure out how to pass the input to ad_narbar so hacking it in here
-# and also added context checking
-set link_list ""
-foreach arg $subnavbar_list {
-        lappend link_list "<a href=\"[lindex $arg 0]\">[lindex $arg 1]</a>"
-# removed context checking because people keep trying to use this navbar to 
-# get to the local parent, which context checking prevents
-#    if { [string match *[lindex $arg 0]* [ad_conn url]] } {
-#        lappend link_list "[lindex $arg 1]"
-#    } else {
-#        lappend link_list "<a href=\"[lindex $arg 0]\">[lindex $arg 1]</a>"
-#    }
-}
-set subnavbar_link "\[[join $link_list " | "]\]"
