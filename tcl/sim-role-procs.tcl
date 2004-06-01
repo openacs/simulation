87f9f7976459fa134891b5a10e3eb9cb41b41ebf
Lars Pind <lars@pinds.com>
2004-06-01 15:13:22 +0000

    Another attempt at getting the outer join right

--- tcl/sim-role-procs.tcl
+++ tcl/sim-role-procs.tcl
@@ -174,12 +174,15 @@ ad_proc -public simulation::role::get {
     db_1row select_sim_role {
         select sr.character_id,
                sr.users_per_case,
-               scx.name as character_name,
-               scx.title as character_title,
-               scx.item_id as character_item_id
+               c.name as character_name,
+               c.title as character_title,
+               c.item_id as character_item_id
         from   sim_roles sr left outer join
-               sim_charactersx scx on (scx.item_id = sr.character_id) left outer join
-               cr_items ci on (ci.item_id = scx.item_id and ci.live_revision = scx.object_id)
+               (select scx.name, scx.title, scx.item_id
+                from   sim_charactersx scx,
+                       cr_items ci 
+                where  ci.item_id = scx.item_id 
+                and    ci.live_revision = scx.object_id) c on (c.item_id = sr.character_id)
         where  sr.role_id = :role_id
     } -column_array local_row
 
